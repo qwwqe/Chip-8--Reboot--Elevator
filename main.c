52d0bb475ce446eaf1b7f9478fa15183c501192f
syphoxy ploxy <spyro.boy@gmail.com>
2010-07-29 10:50:00 -0400

    minor changes and error checking

--- main.c
+++ main.c
@@ -8,20 +8,29 @@
 
 int main(int argc, char **argv) {
 
+ if(argc < 2) {
+  fprintf(stderr, "Missing arguments\n");
+  exit(EXIT_FAILURE);
+ }
+
  mem_t *mem = (mem_t *)malloc(sizeof(mem_t));
  reg_t *reg = (reg_t *)malloc(sizeof(reg_t));
 
  cpu_t cpu = {
-    {opcode0, opcode1, opcode2, opcode3,
-     opcode4, opcode5, opcode6, opcode7,
-     opcode8, opcode9, opcodea, opcodeb,
-     opcodec, opcoded, opcodee, opcodef},
-    mem,
-    reg,
-    0    
+  {
+   opcode0, opcode1, opcode2, opcode3,
+   opcode4, opcode5, opcode6, opcode7,
+   opcode8, opcode9, opcodea, opcodeb,
+   opcodec, opcoded, opcodee, opcodef
+  },
+  mem,
+  reg,
+  0    
  };
 
- if((cpu.mem->size = read_rom(argv[1], &cpu.mem->rom)) < 0) {
+ printf("Initialized CPU: %p\n", &cpu);
+
+ if((cpu.mem->size = read_rom(argv[1], &cpu.mem->rom)) == -1) {
   fprintf(stderr, "Failed to load rom: %s\n", argv[1]);
   exit(EXIT_FAILURE);
  }
@@ -30,17 +39,20 @@ int main(int argc, char **argv) {
 
  cpu.mem->pos = 0;
  while(1) {
-    //cpu.fn[cpu.mem->rom[cpu.mem->pos] >> 1](&cpu);
+  //cpu.fn[cpu.mem->rom[cpu.mem->pos] >> 1](&cpu);
 
-    if(!cpu.advpc)
-        cpu.mem->pos += 2;
-    cpu.advpc = 0;
-  
-/* int x;
- for(x = 0; x < cpu.mem->size; x += 2) {
-  printf("byte #%03d: %02X%02X\n", x, cpu.mem->rom[x], cpu.mem->rom[x + 1]);
-*/
- }
+  if(!cpu.advpc)
+   cpu.mem->pos += 2;
+  cpu.advpc = 0;
+ } 
+ 
+/*
+ * int x;
+ * for(x = 0; x < cpu.mem->size; x += 2) {
+ *  printf("byte #%03d: %02X%02X\n", x, cpu.mem->rom[x], cpu.mem->rom[x + 1]);
+ * }
+ *
+ */
 
  exit(EXIT_SUCCESS);
 }
