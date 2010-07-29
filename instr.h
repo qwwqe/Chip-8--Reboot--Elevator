5641e43126eafa8a1978f00e962134691bd94f87
syphoxy ploxy <spyro.boy@gmail.com>
2010-07-29 09:29:46 -0400

    added new opcode function declarations

--- instr.h
+++ instr.h
@@ -2,6 +2,14 @@
 #ifndef INSTR_H
 #define INSTR_H
 
+void opcode0(cpu_t *cpu); // instructions beginning with 0
+void opcode1(cpu_t *cpu); // instructions beginning with 1
+void opcode2(cpu_t *cpu); // instructions beginning with 2
+void opcode3(cpu_t *cpu); // instructions beginning with 3
+void opcode4(cpu_t *cpu); // instructions beginning with 4
+void opcode5(cpu_t *cpu); // instructions beginning with 5
+void opcode6(cpu_t *cpu); // instructions beginning with 6
+void opcode7(cpu_t *cpu); // instructions beginning with 7
 void opcode8(cpu_t *cpu); // instructions beginning with 8
 void opcode9(cpu_t *cpu); // instructions beginning with 9
 
